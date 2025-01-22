5a34c0dc980b2356d5e1eda8d49bd1bfa9973d67
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-01-22 02:02:44 +0200

    Update locales.lua
    Added timeout locale

--- tpz_discord_whitelist/locales.lua
+++ tpz_discord_whitelist/locales.lua
@@ -2,5 +2,6 @@
 Locales = {}
 
 Locales = {
+  ['TIMEOUT']         = "Connection Timeout! Discord roles could not be found", 
   ['NOT_WHITELISTED'] = "You are not whitelisted, we would like for you to join our discord to create an allowlist application, otherwise you will not be able to join us without the required discord role.",
 }
