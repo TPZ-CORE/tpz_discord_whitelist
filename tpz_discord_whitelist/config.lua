d7eac08eb8b72c36a15d1dfecf8331169a475e8a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-01-23 15:56:47 +0200

    Add files via upload

---
+++ tpz_discord_whitelist/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+---------------------------------------------------------------
+--[[ Discord API Configurations ]]--
+---------------------------------------------------------------
+
+-- The specified discord roles will be considered as administrators and will be used in many scripts.
+-- local hasPermissions = xPlayer.hasAdministratorPermissions() The specified export is used for those permissions.
+Config.DiscordAdministratorRoles  = { 670899926783361024, 222222222222222222 }
+
+-- The specified discord group names will be considered as administrators and will be used in many scripts.
+-- local hasPermissions = xPlayer.hasAdministratorPermissions(group, discordRoles) The specified export is used for those permissions.
+Config.AdministratorGroups = { 'admin' }
