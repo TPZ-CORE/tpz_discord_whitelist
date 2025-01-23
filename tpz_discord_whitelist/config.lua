14f0d2ae7fc19c1d5ea86d69f27c269ac6a25515
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-01-23 15:56:58 +0200

    Update config.lua

--- tpz_discord_whitelist/config.lua
+++ tpz_discord_whitelist/config.lua
@@ -6,7 +6,7 @@ Config = {}
 
 -- The specified discord roles will be considered as administrators and will be used in many scripts.
 -- local hasPermissions = xPlayer.hasAdministratorPermissions() The specified export is used for those permissions.
-Config.DiscordAdministratorRoles  = { 670899926783361024, 222222222222222222 }
+Config.DiscordAdministratorRoles  = { 1111111111111111, 222222222222222222 }
 
 -- The specified discord group names will be considered as administrators and will be used in many scripts.
 -- local hasPermissions = xPlayer.hasAdministratorPermissions(group, discordRoles) The specified export is used for those permissions.
