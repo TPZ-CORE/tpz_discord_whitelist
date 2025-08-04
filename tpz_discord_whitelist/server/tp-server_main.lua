3ea8abc6ee1001755687128eb4c157dabe49cd84
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-08-04 23:35:50 +0300

    Update tp-server_main.lua

--- tpz_discord_whitelist/server/tp-server_main.lua
+++ tpz_discord_whitelist/server/tp-server_main.lua
@@ -2,6 +2,36 @@ local TPZ = exports.tpz_core:getCoreAPI()
 
 local TIMEOUT = 60 -- 1 minute as default for timeout if something goes wrong.
 
+local Queue = {}
+
+-----------------------------------------------------------
+--[[ Local Functions ]]--
+-----------------------------------------------------------
+
+local GetSteamID = function(src)
+    local sid = GetPlayerIdentifiers(src)[1] or false
+
+    if (sid == false or sid:sub(1,5) ~= "steam") then
+        return false
+    end
+
+    return sid
+end
+
+local function RemovePlayerFromQueue(_source)
+    local leavingPosition = Queue[_source] and Queue[_source].position
+    Queue[_source] = nil
+    
+    if leavingPosition then
+        
+        for _, user in pairs(Queue) do
+            if user.position > leavingPosition then
+                user.position = user.position - 1
+            end
+        end
+    end
+end
+
 -----------------------------------------------------------
 --[[ Base Events  ]]--
 -----------------------------------------------------------
@@ -12,6 +42,15 @@ AddEventHandler('playerConnecting', function(name, setKickReason, defer)
     local currentTime  = 0
 
 	defer.defer();
+
+    local steamIdentifier
+
+    -- mandatory wait!
+    Wait(0) 
+
+    steamIdentifier = GetSteamID(_source)
+    -- mandatory wait!
+    Wait(0)
 
     local xPlayer = TPZ.GetPlayer(_source)
 
@@ -51,6 +90,78 @@ AddEventHandler('playerConnecting', function(name, setKickReason, defer)
         return
     end
 
-	defer.done();
+    local insertPosition = 1
+    local now = os.time()
+
+    local priority = Config.DefaultPriority
+
+    if Config.Priorities[steamIdentifier] then
+        priority = Config.Priorities[steamIdentifier]
+    end
+
+    for _, user in pairs(Queue) do
+        if priority > user.priority then
+            insertPosition = math.max(insertPosition, user.position + 1)
+
+        elseif priority == user.priority and now > (user.joinedAt or now) then
+            insertPosition = math.max(insertPosition, user.position + 1)
+        end
+    end
+
+    -- Shift others down
+    for _, user in pairs(Queue) do
+        if user.position >= insertPosition then
+            user.position = user.position + 1
+        end
+    end
+
+    -- Add to queue
+    Queue[_source] = {
+        source   = _source,
+        position = insertPosition,
+        priority = priority,
+        joinedAt = now,
+        waiting  = 0,
+        defer    = defer
+    }
+
+end)
+
+-----------------------------------------------------------
+--[[ Threads ]]--
+-----------------------------------------------------------
+
+Citizen.CreateThread(function() 
+
+    while true do
+
+        Wait(1000)
+
+        local length = TPZ.GetTableLength(Queue)
+
+        if length > 0 then
+
+            local maxPlayers     = Config.sv_maxclients
+            local currentPlayers = TPZ.GetTableLength(GetPlayers())
+
+            for _, user in pairs (Queue) do
+
+                user.waiting = user.waiting + 1
+
+                user.defer.update(string.format(Locales['CHECKING_PRIORITY_POSITION'], user.position, length, user.waiting))
+
+                if currentPlayers < maxPlayers and user.position <= 1 then
+                    RemovePlayerFromQueue(user.source)
+                    user.defer.done();
+                end
+
+                if GetPlayerName(user.source) == nil then
+                    RemovePlayerFromQueue(user.source)
+                end
+
+            end
+
+        end
+    end
 
 end)
