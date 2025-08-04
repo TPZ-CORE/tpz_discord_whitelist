3224210441bff9302c9061550392a14f4327b427
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-08-04 23:35:28 +0300

    Update locales.lua

--- tpz_discord_whitelist/locales.lua
+++ tpz_discord_whitelist/locales.lua
@@ -2,6 +2,7 @@
 Locales = {}
 
 Locales = {
-  ['TIMEOUT']         = "Connection Timeout! Discord roles could not be found", 
-  ['NOT_WHITELISTED'] = "You are not whitelisted, we would like for you to join our discord to create an allowlist application, otherwise you will not be able to join us without the required discord role.",
+  ['TIMEOUT']                    = "Connection Timeout! Discord roles could not be found", 
+  ['NOT_WHITELISTED']            = "You are not whitelisted, we would like for you to join our discord to create an allowlist application, otherwise you will not be able to join us without the required discord role.",
+  ['CHECKING_PRIORITY_POSITION'] = "Your position in the queue is %s / %s players and currently waiting %s seconds.",
 }
